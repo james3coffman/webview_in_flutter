f786f1cbe63bde86e589e88c601546b6698a1c3d
James Coffman <james@lightsouttattoo.com>
2022-01-26 00:32:11 -0500

    Update
    added files structure

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+
+import 'package:flutter/material.dart';
+
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'src/menu.dart';                                // Add this import
+import 'src/navigation_controls.dart';
+import 'src/web_view_stack.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final controller = Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Igniting Faith'),
+        backgroundColor: Colors.black,
+
+
+        actions: [
+          NavigationControls(controller: controller),
+          Menu(controller: controller),                // Add this line
+        ],
+      ),
+      body: WebViewStack(controller: controller),
+    );
+  }
+}
